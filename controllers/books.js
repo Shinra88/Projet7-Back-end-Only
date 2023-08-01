a9f82c72a974fd00d7ca021d571b561faa7c273e
Kev COLIN <colin-kevin@outlook.fr>
2023-08-01 14:00:07 +0200

    vercel var

--- controllers/books.js
+++ controllers/books.js
@@ -8,7 +8,7 @@ exports.createBook = (req, res, next) => {
     const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+        imageUrl: `${process.env.IMAGES_HOST}/images/${req.file.filename}`,
         averageRating: bookObject.ratings[0].grade
     });
   
@@ -21,7 +21,7 @@ exports.createBook = (req, res, next) => {
   
     const bookObject = req.file ? {
         ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+        imageUrl: `${process.env.IMAGES_HOST}/images/${req.file.filename}`
     } : { ...req.body }
 
     delete bookObject._userId;
